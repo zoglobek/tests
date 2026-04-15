2cc509c21ac554d9f229050153cf0e830b1857d1
gal <newmangal@msn.com>
2026-04-15 19:00:53 +0300

    made helloworld flask

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,25 @@
+from flask import Flask, Response
+import cowsay
+import io
+from contextlib import redirect_stdout
 
-print("hello world")
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    f = io.StringIO()
+    with redirect_stdout(f):
+        cowsay.cow("Hello, World!")
+    output = f.getvalue()
+    
+    return f"""
+    <html>
+        <head><title>Cowsay Hello</title></head>
+        <body style="background-color: #121212; color: #00ff00; padding: 20px;">
+            <pre>{output}</pre>
+        </body>
+    </html>
+    """
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000, debug=True)
